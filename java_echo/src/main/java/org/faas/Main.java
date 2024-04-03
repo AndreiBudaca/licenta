5b5ea4797ae8392cac0a0679c97ff8bfd74ba708
AndreiBudaca <marius-andrei.budaca@student.tuiasi.ro>
2024-04-03 21:10:50 +0300

    Add sleep to 'compute' function

--- java_echo/src/main/java/org/faas/Main.java
+++ java_echo/src/main/java/org/faas/Main.java
@@ -1,9 +1,12 @@
 package org.faas;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class Main {
-    public String main(String arg) {
+    public String main(String arg) throws InterruptedException {
+        Random rand = new Random();
+        Thread.sleep(200 + rand.nextInt(800));
         return Objects.requireNonNullElse(arg, "Input was empty");
     }
 }
