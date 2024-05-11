54fd331d2578e19c59ea261ef867584fcbd89dfc
AndreiBudaca <marius-andrei.budaca@student.tuiasi.ro>
2024-05-11 14:13:35 +0300

    Configure task dispacher to run deployments with the correspondent FAAS image

--- task_dispacher/src/main/java/org/example/Main.java
+++ task_dispacher/src/main/java/org/example/Main.java
@@ -1,11 +1,25 @@
 package org.example;
 
+import io.kubernetes.client.openapi.ApiException;
 import org.example.communication.KubernetesCommunication;
 import org.example.communication.RedisCommunication;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         //RedisCommunication redis = RedisCommunication.getInstance();
-        KubernetesCommunication c = new KubernetesCommunication();
+        try {
+            KubernetesCommunication c = new KubernetesCommunication();
+        }
+        catch (ApiException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getCode());
+            System.out.println(e.getResponseBody());
+        }
+        catch (Exception e) {
+            System.out.println("An exception has occured");
+            System.out.println(e.getMessage());
+        }
     }
 }
--- task_dispacher/src/main/java/org/example/communication/KubernetesCommunication.java
+++ task_dispacher/src/main/java/org/example/communication/KubernetesCommunication.java
@@ -1,66 +1,107 @@
 package org.example.communication;
 
-import io.fabric8.kubernetes.api.model.ServiceAccount;
-import io.fabric8.kubernetes.api.model.ServiceAccountBuilder;
-import io.fabric8.kubernetes.api.model.apps.Deployment;
-import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
-import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.AppsV1Api;
+import io.kubernetes.client.openapi.models.*;
+import io.kubernetes.client.util.ClientBuilder;
+import io.kubernetes.client.util.credentials.Authentication;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class KubernetesCommunication {
-    private static final String NAMESPACE = "default";
 
-    public KubernetesCommunication() {
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in));
+    private final AppsV1Api api;
 
+    public KubernetesCommunication() throws IOException, ApiException {
 
-        try (KubernetesClient client = new KubernetesClientBuilder().build()) {
-            Deployment deployment = new DeploymentBuilder()
-                    .withNewMetadata()
-                    .withName("nginx")
-                    .endMetadata()
-                    .withNewSpec()
-                    .withReplicas(1)
-                    .withNewTemplate()
-                    .withNewMetadata()
-                    .addToLabels("app", "nginx")
-                    .endMetadata()
-                    .withNewSpec()
-                    .addNewContainer()
-                    .withName("nginx")
-                    .withImage("nginx")
-                    .addNewPort()
-                    .withContainerPort(80)
-                    .endPort()
-                    .endContainer()
-                    .endSpec()
-                    .endTemplate()
-                    .withNewSelector()
-                    .addToMatchLabels("app", "nginx")
-                    .endSelector()
-                    .endSpec()
-                    .build();
+        ApiClient client = System.getenv("REDIS_PORT") != null ?
+                ClientBuilder.cluster().build() :
+                ClientBuilder.defaultClient();
 
-            deployment = client.apps().deployments().inNamespace(NAMESPACE).resource(deployment).create();
-            System.out.println("Created deployment: " + deployment);
+        Configuration.setDefaultApiClient(client);
+        api = new AppsV1Api();
 
-            Thread.sleep(5000);
-
-            System.out.println("Scaling up: " + deployment.getMetadata().getName());
-            client.apps().deployments().inNamespace(NAMESPACE).withName("nginx").scale(2, true);
-            System.out.println("Created replica sets: " + client.apps().replicaSets().inNamespace(NAMESPACE).list().getItems());
-
-            Thread.sleep(5000);
-
-            System.out.println("Deleting: " + deployment.getMetadata().getName());
-            client.resource(deployment).delete();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        System.out.println("Available authentications");
+        for(String auth : client.getAuthentications().keySet()) {
+            System.out.println(auth);
         }
+    }
+
+    public void CreateDeployment() throws InterruptedException, ApiException {
+        V1Deployment d = new V1Deployment();
+        {
+            d.setApiVersion("apps/v1");
+            d.setKind("Deployment");
+
+            V1ObjectMeta dMeta = new V1ObjectMeta();
+            {
+                dMeta.setName("nginx-deployment");
+                dMeta.setLabels(new HashMap<>() {{
+                    put("app", "nginx");
+                }});
+            }
+            d.setMetadata(dMeta);
+
+            V1DeploymentSpec dSpec = new V1DeploymentSpec();
+            {
+                dSpec.setReplicas(1);
+
+                V1LabelSelector dSpecSelector = new V1LabelSelector();
+                {
+                    dSpecSelector.setMatchLabels(new HashMap<>() {{
+                        put("app", "nginx");
+                    }});
+                }
+                dSpec.setSelector(dSpecSelector);
+
+                V1PodTemplateSpec dSpecTemplate = new V1PodTemplateSpec();
+                {
+                    V1ObjectMeta dSpecTemplateMeta = new V1ObjectMeta();
+                    {
+                        dSpecTemplateMeta.setLabels(new HashMap<>() {{
+                            put("app", "nginx");
+                        }});
+                    }
+                    dSpecTemplate.setMetadata(dSpecTemplateMeta);
+
+                    V1PodSpec dSpecTemplateSpec = new V1PodSpec();
+                    {
+                        V1Container dSpecTemplateSpecContainer = new V1Container();
+                        {
+                            dSpecTemplateSpecContainer.setName("nginx");
+                            dSpecTemplateSpecContainer.setImage("nginx:1.14.2");
+
+                            V1ContainerPort dSpecTemplateSpecContainerPort = new V1ContainerPort();
+                            {
+                                dSpecTemplateSpecContainerPort.setContainerPort(80);
+                            }
+                            dSpecTemplateSpecContainer.setPorts(new ArrayList<>() {{ add(dSpecTemplateSpecContainerPort); }});
+                        }
+                        dSpecTemplateSpec.setContainers(new ArrayList<>() {{ add(dSpecTemplateSpecContainer); }});
+                    }
+                    dSpecTemplate.setSpec(dSpecTemplateSpec);
+                }
+                dSpec.setTemplate(dSpecTemplate);
+            }
+            d.setSpec(dSpec);
+        }
+
+        api.createNamespacedDeployment("default", d, null, null, null, null);
+        System.out.println("Deployment created!");
+
+        V1DeploymentList deploymentList = api.listNamespacedDeployment("default", null,
+                null, null, null, null, null, null, null, null, null);
+        for (V1Deployment deployment : deploymentList.getItems()) {
+            System.out.println(deployment.getMetadata().getName());
+        }
+
+        Thread.sleep(5000);
+
+        api.deleteNamespacedDeployment("nginx-deployment", "default",
+                null, null, null, null, null, null);
     }
 }
